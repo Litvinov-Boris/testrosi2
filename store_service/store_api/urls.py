ecd24c064811645bb9b343c4ffd1e4e15d23ab2c
Litvinov-Boris <boris.litvinov2017@yandex.ru>
2020-12-03 02:00:51 +0300

    1

--- store_service/store_api/urls.py
+++ store_service/store_api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('<str:userUid>/orders', views.get_orders),
+    path('<str:userUid>/purchase', views.get_orders),
     path('<str:userUid>/<str:orderUid>', views.get_order),
     path('<str:userUid>/<str:orderUid>/warranty', views.req_warranty),
-    path('<str:userUid>/purchase', views.get_orders),
     path('<str:userUid>/<str:orderUid>/refund', views.req_warranty)
 ]
